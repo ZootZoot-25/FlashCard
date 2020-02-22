1eb04f14298662efc077bc9c22e82103bc230cf8
Gzzuni5 <gzzuni5@gmail.com>
2020-02-21 21:00:46 -0800

    Added color, as well as multiple questions, and highlight if thw question is correct or incorrect

--- app/src/main/java/josegonzalez/com/MainActivity.java
+++ app/src/main/java/josegonzalez/com/MainActivity.java
@@ -12,10 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.Background).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.Original_Answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.torquoise));
+                findViewById(R.id.flashcard_incorrectAns1).setBackgroundColor(getResources().getColor(R.color.torquoise));
+                findViewById(R.id.flashcard_incorrectAns2).setBackgroundColor(getResources().getColor(R.color.torquoise));
+            }
+        });
+
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.Original_Answer).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
             }
         });
@@ -23,9 +34,25 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.green));
             }
         });
+
+        findViewById(R.id.flashcard_incorrectAns1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.green));
+                findViewById(R.id.flashcard_incorrectAns1).setBackgroundColor(getResources().getColor(R.color.red));
+            }
+        });
+
+        findViewById(R.id.flashcard_incorrectAns2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.green));
+                findViewById(R.id.flashcard_incorrectAns2).setBackgroundColor(getResources().getColor(R.color.red));
+            }
+        });
+
     }
 }
